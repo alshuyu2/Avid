334d2f98a15c26e6810ec04d4fb68c34bde0598a
sconger123 <conger.sam1822@yahoo.com>
2022-02-01 11:29:14 -0600

    added new class

---
+++ CapstoneProject595/user.py
@@ -0,0 +1,8 @@
+class User():
+
+    def __init__(self, name=None, height=0, weight=0, age=0, sex=0):
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.age = age
+        self.sex = sex
