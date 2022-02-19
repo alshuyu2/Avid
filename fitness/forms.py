fa2756fe3608e660b24d2dc0ab30407ff13a2e8d
sconger123 <conger.sam1822@yahoo.com>
2022-02-19 17:23:51 -0600

    registering now takes a name field

--- fitness/forms.py
+++ fitness/forms.py
@@ -5,15 +5,17 @@ from .models import MyUser
 
 class MyUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    name = forms.CharField(max_length=60)
 
     class Meta:
         model = MyUser
-        fields = ('email',)
+        fields = ('email', 'name')
         # ('name', 'height_in_inches', "weight_in_pounds", 'age', 'sex')
 
         def save(self, commit=True):
             user = super(MyUserCreationForm, self).save(commit=False)
             user.email = self.cleaned_data['email']
+            user.name = self.cleaned_data['name']
             if commit:
                 user.save()
             return user
