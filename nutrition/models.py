570d45380054d59c2defc07afdb3bbdec276c998
alshuyu2 <alshuyu2@uwm.edu>
2022-04-06 19:29:48 -0500

    Have nutrition and it's models in it's own app folder.

--- nutrition/models.py
+++ nutrition/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 #What data should the nutrition model have?
@@ -12,15 +11,7 @@ from django.db import models
 #You can have a diet tracking model
 
 #For now  have a simple food model called food dictionary model that has all the necessary food items
-class FoodDictionary(models.Model):
-    #Name
-    name = models.CharField(max_length=50)
-    #Nutrition information (Carbohydrates, fats, protein, and all the vitamins and minerals)
-    calories = models.IntegerField #start with calories for now
-    #Serving size (number of the amount, lbs, grams for solid food, liter for liquid drinks)
-    serving = models.IntegerField
-    foodType = models.BooleanField #put true if it's solid, false if it's liquid
-
+from fitness.models import MyUser
 
 class Meal(models.Model):
     # user = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
@@ -29,10 +20,11 @@ class Meal(models.Model):
     protein = models.IntegerField(null=True)
     fat = models.IntegerField(null=True)
     carbs = models.IntegerField(null=True)
-    serving_size = models.CharField(max_length=50, null=True)
+    serving_size = models.IntegerField()
+    serving_unit = models.CharField(max_length=50) #example: ounce, unit
 
 
 class UserMeal(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
-    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, null=True)
-    servings = models.IntegerField(null=True)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    servings = models.IntegerField()
