120802818f32457e1cfd94a0c8cd6a0cd39fe821
XinQiangQui <xxqui@uwm.edu>
2022-04-08 06:48:20 -0500

    .

--- fitness/migrations/0002_auto_20220407_1317.py
+++ fitness/migrations/0002_auto_20220407_1317.py
@@ -2,12 +2,24 @@
 
 from django.db import migrations
 from django.contrib.auth.hashers import make_password as pw_hash
-from fitness.models import MyUser
+from fitness.models import MyUser, Exercise
 
 
 def add_superuser(a, b):
     a = MyUser(name='admin', email='admin@admin.com', password=pw_hash('admin'), is_staff=True, is_superuser=True)
     a.save()
+
+
+def add_exercise(a, b):
+    ex1 = Exercise.create_ex("Bird Dog", 100, "None", "None",
+                             "media/images/birddog.jpg",
+                             "This is the description for birddog")
+    ex2 = Exercise.create_ex("Forward Lunge ", 200, "None", "None",
+                             "media/images/forward_lunge.jpg",
+                             "This is the description for forward lunge")
+    ex3 = Exercise.create_ex("Ankle Flexion", 300, "None", "None",
+                             "media/images/ankle_flexion.jpg",
+                             "This is the description for ankle flexion")
 
 
 class Migration(migrations.Migration):
